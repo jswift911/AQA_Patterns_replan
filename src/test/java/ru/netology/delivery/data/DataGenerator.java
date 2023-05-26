7aca89cc6818937dfbadca2c1c94f63eecd84491
Alex <jswift888@gmail.com>
2023-05-26 14:56:11 +0300

    init

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,53 @@
+package ru.netology.delivery.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        LocalDate date = LocalDate.now().plusDays(shift);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return date.format(formatter);
+    }
+
+    public static String generateCity(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.address().city();
+    }
+
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.name().firstName() + " " + faker.name().lastName();
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            String city = generateCity(locale);
+            String name = generateName(locale);
+            String phone = generatePhone(locale);
+            return new UserInfo(city, name, phone);
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
---
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -0,0 +1,55 @@
+package ru.netology.delivery.test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import ru.netology.delivery.data.DataGenerator;
+
+import java.time.Duration;
+import java.util.concurrent.locks.Condition;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.delivery.data.DataGenerator.generateDate;
+
+class DeliveryTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
+        //Configuration.holdBrowserOpen = true;
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = generateDate(daysToAddForSecondMeeting);
+
+        $("[data-test-id='city'] .input__control").setValue(validUser.getCity());
+        $("[data-test-id='date'] .input__control").click();
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").setValue(firstMeetingDate);
+        $("[data-test-id='name'] .input__control").setValue(validUser.getName());
+        $("[data-test-id='phone'] [name='phone']").setValue(validUser.getPhone());
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(byText("Запланировать")).click();
+        $(By.cssSelector("[data-test-id='success-notification'] .notification__content")).shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate), Duration.ofMillis(15000)).shouldBe(visible);
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").setValue(secondMeetingDate);
+        $(byText("Запланировать")).click();
+        $(By.cssSelector("[data-test-id='replan-notification'] .notification__content")).shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"), Duration.ofMillis(15000)).shouldBe(visible);
+        $(byText("Перепланировать")).click();
+        $(By.cssSelector("[data-test-id='success-notification'] .notification__content")).shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate), Duration.ofMillis(15000)).shouldBe(visible);
+    }
+}
